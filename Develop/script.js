8ca0eb2abfa7bff04102ce8737f8ca2bc19d4c04
Christine Angulo <christineangulo00@gmail.com>
2021-08-13 17:27:03 -0600

    window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -13,10 +13,15 @@ function writePassword() {
 
 }
 
+function generatePassword(length, upper, lower, number, symbol) {
+  // choosing the length of the password
+  var length = window.prompt('Choose a length for your password! (Must be between 8 & 128 characters)')
+  var upper = window.prompt('Would you like to include uppercase letters? Type "YES" or "NO"')
+  var lower = window.prompt('Would you like to include lowercase letters? Type "YES" or "NO"')
+  var number = window.prompt('Would you like to include numbers? Type "YES" or "NO"')
+  var symbol = window.prompt('Would you like to include symbols? Type "YES" or "NO"')
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// generate functions
-
-// window display promts
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Password Generator Starter Code
+# Password Generator
+
+generating a password! when the user clicks the generate button, they're presented with prompts to ask how long they want the password to be (between 8 and 128 characters)
+
+once they specify length, the user is presented with window promts to confirm if they want lowercase letters, uppercase letters, numbers, or symbols. once the user specifies their requirements, a random password is presented in the password field.
